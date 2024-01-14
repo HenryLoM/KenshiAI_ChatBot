92cc1f6bb4839d316cce0bf7e13d512969376cb7
Muhammad <144126161+HenryLoM@users.noreply.github.com>
2024-01-14 08:46:34 +0300

    Update #4: updated main.py and FunctionalPart.py - made GUI

--- MainDirectory_Object-9/FunctionalPart.py
+++ MainDirectory_Object-9/FunctionalPart.py
@@ -1,70 +1,34 @@
 from characterai import PyCAI
 
 # variables from chatbot's library
-client = PyCAI('e8fef74a7ca89d9ae65d2606591fb94b0c323b9c')  # profile for work
-char = 'IlJEFcRpD74cqg8mNMpM5iTXFF5EtUI66yWbT9we2Wo'  # character for work
-chat = client.chat.get_chat(char)  # import chat into variable
-participants = chat['participants']
-if not participants[0]['is_human']:
-    tgt = participants[0]['user']['username']
+client = PyCAI("e8fef74a7ca89d9ae65d2606591fb94b0c323b9c")  # profile for work
+char = "IlJEFcRpD74cqg8mNMpM5iTXFF5EtUI66yWbT9we2Wo"  # character for work
+chat = client.chat.get_chat(char)  # import a chat into the variable
+participants = chat["participants"]
+if not participants[0]["is_human"]:
+    tgt = participants[0]["user"]["username"]
 else:
-    tgt = participants[1]['user']['username']
+    tgt = participants[1]["user"]["username"]
 
+# class of our ChatBot
 class ChatBot:
 
     def __init__(self):
-        self.topics = []
-        self.conversations = []
+        self.save_message = "File with all conversation was created on your desktop"
+        self.start_message = f'''
+_ _ _ _ _ _ _ _ _ ChatBot Demo _ _ _ _ _ _ _ _ _      
+Commands:
+/new – end the current chat and make a new one
+/save – save the current chat as a file
+_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _'''
 
-    def gui(self):
-        print(f'''
-_ _ _ _ _ {self.topics} _ _ _ _ _
-          ChatBot Demo          
-Management:
-/new – end this chat and make a new
-/end – stop using ChatBot
-/read - comeback to the topic to read a conversation you made before'
-_ _ _ _ _ _ _ _ _ _ _ _''')
-
-    def chat_bots_answer(self, prompt):
+    def get_chatbots_answer(self, prompt):
         message = prompt  # get a message
-        data = client.chat.send_message(chat['external_id'], tgt, message)  # mix all our stuff
-        text = data['replies'][0]['text']  # get ready the text
+        data = client.chat.send_message(chat["external_id"], tgt, message)  # mix all our stuff
+        text = data["replies"][0]["text"]  # get ready the text
         return f"ChatBot: {text}"  # return an answer
 
-    def new_topic(self, name, conv):
-        self.topics.append(name)
-        self.conversations.append(conv)
-
-    def overall(self):
-        print()
-        print('_ _ _ _ _ All topics you made _ _ _ _ _')
-        for i in range(len(self.topics)):
-            print(i + 1, "–", self.topics[i])
-
-    def comeback(self):
-        if not self.topics:
-            print("You can't use /read because it's your first conversation")
-        else:
-            print()
-            print('_ _ _ _ _ Give the number of a necessary one _ _ _ _ _')
-            for i in range(len(self.topics)):
-                print(i + 1, "–", self.topics[i])
-            while True:
-                try:
-                    select = int(input()) - 1
-                    if 0 <= select < len(self.topics):
-                        break  # to stop the cycle if all is good
-                    else:
-                        print("Invalid number. Please enter a valid number.")
-                except ValueError:
-                    print("Invalid input. Please enter a valid number.")
-
-            print(f'_ _ _ Chat #{select + 1}: {self.topics[select]}')
-            conversation = self.conversations[select]
-            for i in range(0, len(conversation), 2):
-                print(f"User – {conversation[i]}")
-                if i + 1 < len(conversation):
-                    print(f"ChatBot – {conversation[i + 1]}")
-            print('_ _ _')
-            print("_ _ _ _ _ It's read mode only, you still in the last topic and can continue _ _ _ _ _")
+    def make_file(self, conversation):
+        with open("chatbot's_data", "w") as file:  # make the file
+            for key, value in conversation.items():  # convert a conversation
+                file.write(f"User: {key}\n{value}\n")  # fill the file
--- MainDirectory_Object-9/main.py
+++ MainDirectory_Object-9/main.py
@@ -1,45 +1,75 @@
 from FunctionalPart import ChatBot
+import tkinter as tk
 
 # make the object ChatBot
 bot = ChatBot()
 
 # variables
-total_messages = 0
-conversation = []
-first_prompt = None
+first_message = True
+conversation = {}
 
-# working part
-while True:
-    # Make GUI before any message
-    if total_messages == 0:
-        bot.gui()
+# functions
+def greetings():
+    global first_message
+    if first_message:
+        first_message = False
+        text_widget.config(state=tk.NORMAL)  # start chat
+        text_widget.insert(tk.END, bot.start_message + "\n\n")  # show chatbot's message
+        text_widget.config(state=tk.DISABLED)  # stop chat for saving it of editing
 
-    # Get user's prompt
-    prompt = input('User – ')
-    total_messages += 1
-    conversation.append(prompt)
-
-    # Save the first message as a name of the topic
-    if total_messages == 1:
-        first_prompt = prompt
-
-    # Processing commands
+def on_button_click(event=None):
+    global conversation
+    prompt = entry.get()
 
     # Command /new
-    if prompt.lower() == '/new':
-        total_messages = 0
-        bot.new_topic(first_prompt, conversation)
-        conversation = []
-    # Command /read
-    elif prompt.lower() == '/read':
-        bot.comeback()
+    if prompt == "/new":
+        conversation.clear()
+        # update the widget of the window
+        text_widget.config(state=tk.NORMAL)  # start chat
+        text_widget.delete(1.0, tk.END)  # Clear the entire content of the text_widget
+        text_widget.insert(tk.END, bot.start_message + "\n\n")  # show chatbot's message
+        text_widget.config(state=tk.DISABLED)  # stop chat for saving it of editing
+        entry.delete(0, tk.END)  # clear the input place
+
     # Command /end
-    elif prompt.lower() == '/end':
-        bot.new_topic(first_prompt, conversation)
-        bot.overall()
-        break
+    elif prompt == "/save":
+        bot.make_file(conversation)
+        # update the widget of the window
+        text_widget.config(state=tk.NORMAL)  # start chat
+        text_widget.insert(tk.END, bot.save_message + "\n\n")  # show chatbot's message
+        text_widget.config(state=tk.DISABLED)  # stop chat for saving it of editing
+        entry.delete(0, tk.END)  # clear the input place
+
     # Simple message
     else:
-        answer = bot.chat_bots_answer(prompt)
-        print(answer)
-        conversation.append(answer)
+        answer = bot.get_chatbots_answer(prompt)
+        conversation[prompt] = answer
+        # update the widget of the window
+        text_widget.config(state=tk.NORMAL)  # start chat
+        text_widget.insert(tk.END, f"User: {prompt}" + "\n\n")  # show user's message
+        text_widget.insert(tk.END, answer + "\n\n")  # show chatbot's message
+        text_widget.config(state=tk.DISABLED)  # stop chat for saving it of editing
+        entry.delete(0, tk.END)  # clear the input place
+
+# make the window
+root = tk.Tk()
+root.title("ChatBot")
+root.geometry("500x350")
+
+# chat
+text_widget = tk.Text(root, wrap="word")
+text_widget.pack(expand=True, fill="both")
+text_widget.config(state=tk.DISABLED)
+greetings()
+
+# input place
+entry = tk.Entry(root)
+entry.pack(side="bottom", fill="x", padx=(0, 50), pady=1)
+entry.bind("<Return>", on_button_click)  # "Enter / Return" can be used for sending messages
+
+# button
+button = tk.Button(root, text=">", width=2, height=1, pady=2, command=on_button_click)
+button.place(relx=1, rely=1, anchor="se")
+
+# create the window
+root.mainloop()
